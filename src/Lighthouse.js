0f92a7e545320e2804e1b2c9af9ea877cecb8c4f
Luis Montes <monteslu@gmail.com>
2013-03-17 08:33:20 -0700

    init

---
+++ src/Lighthouse.js
@@ -0,0 +1,21 @@
+define([
+  'dcl',
+  'frozen/box2d/CircleEntity',
+  'frozen/plugins/loadImage!images/lighthouse.png'
+], function(dcl, Circle, img){
+
+  'use strict';
+
+  return dcl(Circle, {
+
+    x: 159,
+    y: 258,
+    radius: 56.356011214421486,
+    staticBody: true,
+    id: 'lighthouse',
+    draw: function(ctx){
+      ctx.drawImage(img, 102, 60, img.width, img.height);
+    }
+  });
+
+});
---
+++ src/Zombie.js
@@ -0,0 +1,15 @@
+define([
+  'dcl',
+  './Creature',
+  'frozen/plugins/loadImage!images/greenZombie/walking_b.png'
+], function(dcl, Creature, img){
+
+  'use strict';
+
+  return dcl(Creature, {
+
+    img: img
+
+  });
+
+});
